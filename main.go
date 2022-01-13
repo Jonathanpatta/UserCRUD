8f0250d075677492f783d053de41b8ccec6eecd7
JonathanPatta <jonathan.patta@gmail.com>
2022-01-13 16:08:05 +0530

    create user

--- main.go
+++ main.go
@@ -1,23 +1,46 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"github.com/google/uuid"
+)
 
 type User struct {
 	EmailAddress string
 	FirstName    string
 	LastName     string
-	PhoneNumber  int64
+	PhoneNumber  string
 	DOB          string
 	UUID         string
 }
 
 var UserStore []User
 
-func CreateUser(emailAddr string, firstName string, lastName string, phoneNumber int64, dob string) (user User, err error) {
+func CreateUser(emailAddr string, firstName string, lastName string, phoneNumber string, dob string) (user User, err error) {
 	if emailAddr == "" {
-		err = errors.New("Email Address cannot be empty")
+		err = errors.New("email address cannot be empty")
+		return user, err
+	} else if firstName == "" {
+		err = errors.New("first name cannot be empty")
+		return user, err
 	}
+	id, idCreationError := uuid.NewRandom()
+	if idCreationError != nil {
+		return user, idCreationError
+	}
+	user.UUID = id.String()
+	user.EmailAddress = emailAddr
+	user.FirstName = firstName
+	user.LastName = lastName
+	user.PhoneNumber = phoneNumber
+	user.DOB = dob
+
+	UserStore = append(UserStore, user)
+
+	return user, err
 }
 
 func main() {
+	fmt.Println(CreateUser("jonathan.patta@gmail.com", "jonathan", "patta", "", ""))
 }
