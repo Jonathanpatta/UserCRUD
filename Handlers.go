a152e694b81ab0eac25b60f1bcdad32766a9c431
JonathanPatta <jonathan.patta@gmail.com>
2022-01-18 16:05:28 +0530

    CreateUserHandler implemented

--- Handlers.go
+++ Handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -13,11 +14,21 @@ func PingHandler() func(rw http.ResponseWriter, r *http.Request) {
 
 func CreateUserHandler() func(rw http.ResponseWriter, r *http.Request) {
 	return func(rw http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case "POST":
-			fmt.Fprintf(rw, "create user")
-		default:
-			http.Error(rw, "404 not found.", http.StatusNotFound)
+		r.ParseForm()
+		email := r.Form.Get("email")
+		firstName := r.Form.Get("firstname")
+		lastName := r.Form.Get("lastname")
+		phoneNumber := r.Form.Get("phonenumber")
+		dateOfBirth := r.Form.Get("dob")
+
+		user, err := CreateUser(email, firstName, lastName, phoneNumber, dateOfBirth)
+		if err != nil {
+			fmt.Fprintf(rw, err.Error())
+			rw.WriteHeader(http.StatusInternalServerError)
+			return
 		}
+
+		encoder := json.NewEncoder(rw)
+		encoder.Encode(user)
 	}
 }
--- Router.go
+++ Router.go
@@ -3,15 +3,30 @@ package main
 import "net/http"
 
 type Handler struct {
-	EndPoint string
-	Method   string
-	handler  func(http.ResponseWriter, *http.Request)
+	EndPoint        string
+	Method          string
+	HandlerFunction func(http.ResponseWriter, *http.Request)
 }
 
 type Router struct {
 	Handlers []*Handler
 }
 
+func (router *Router) RouterMiddleWare(rw http.ResponseWriter, r *http.Request) {
+	for _, handler := range router.Handlers {
+		if handler.EndPoint == r.URL.Path && handler.Method == r.Method {
+			handler.HandlerFunction(rw, r)
+		}
+	}
+}
+
 func (r *Router) NewRouter() *Router {
+	http.HandleFunc("/", r.RouterMiddleWare)
 	return &Router{}
+}
+
+func (r *Router) RegisterHandler(endpoint string, method string, fn func(http.ResponseWriter, *http.Request)) {
+	newHandler := Handler{EndPoint: endpoint, Method: method, HandlerFunction: fn}
+
+	r.Handlers = append(r.Handlers, &newHandler)
 }
