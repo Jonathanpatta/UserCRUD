a73697af91ac749f81c15d575551b100a60fd982
JonathanPatta <jonathan.patta@gmail.com>
2022-01-19 19:09:08 +0530

    ClientUpdate Test implemented

--- client_test.go
+++ client_test.go
@@ -89,5 +89,69 @@ func TestClientGetUser(t *testing.T) {
 }
 
 func TestClientUpdateUser(t *testing.T) {
+	endPoint := BaseUrl + "/users"
 
+	data := url.Values{}
+	data.Set("email", "jonathan.patta@gmail.com")
+	data.Set("firstname", "Jonathan")
+
+	payload := strings.NewReader(data.Encode())
+	req, _ := http.NewRequest("POST", endPoint, payload)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	bodyData := string(body)
+
+	// fmt.Println(bodyData)
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Error while creating user:" + bodyData)
+	}
+
+	var user User
+
+	// err = json.NewDecoder(resp.Body).Decode(&user)
+	err = json.Unmarshal(body, &user)
+
+	if err != nil {
+		fmt.Println(err)
+		t.Error()
+	}
+	// fmt.Println(user)
+	id := user.UUID
+
+	endPoint = BaseUrl + "/users/" + id
+
+	// fmt.Println(endPoint)
+
+	data = url.Values{}
+	data.Set("email", user.EmailAddress)
+	data.Set("firstname", user.FirstName)
+	data.Set("LastName", user.LastName)
+	data.Set("DOB", user.DOB)
+	data.Set("PhoneNumber", user.PhoneNumber)
+	data.Set("UUID", user.UUID)
+
+	payload = strings.NewReader(data.Encode())
+	req, _ = http.NewRequest("PUT", endPoint, payload)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	resp, err = client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, _ = ioutil.ReadAll(resp.Body)
+	bodyData = string(body)
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Error while Updating User: " + bodyData)
+	}
 }
