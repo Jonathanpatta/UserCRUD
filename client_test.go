fac1be426c9e9050f064e3488779f2c840464f7b
JonathanPatta <jonathan.patta@gmail.com>
2022-01-19 19:17:51 +0530

    ClientDelete Test implemented

--- client_test.go
+++ client_test.go
@@ -155,3 +155,64 @@ func TestClientUpdateUser(t *testing.T) {
 		t.Errorf("Error while Updating User: " + bodyData)
 	}
 }
+
+func TestClientDeleteUser(t *testing.T) {
+	endPoint := BaseUrl + "/users"
+
+	data := url.Values{}
+	data.Set("email", "jonathan.patta@gmail.com")
+	data.Set("firstname", "Jonathan")
+
+	payload := strings.NewReader(data.Encode())
+	req, _ := http.NewRequest("POST", endPoint, payload)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	bodyData := string(body)
+
+	// fmt.Println(bodyData)
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Error while creating user:" + bodyData)
+	}
+
+	var user User
+
+	// err = json.NewDecoder(resp.Body).Decode(&user)
+	err = json.Unmarshal(body, &user)
+
+	if err != nil {
+		fmt.Println(err)
+		t.Error()
+	}
+	// fmt.Println(user)
+	id := user.UUID
+
+	endPoint = BaseUrl + "/users/" + id
+
+	// fmt.Println(endPoint)
+
+	data = url.Values{}
+	payload = strings.NewReader(data.Encode())
+	req, _ = http.NewRequest("DELETE", endPoint, payload)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	resp, err = client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, _ = ioutil.ReadAll(resp.Body)
+	bodyData = string(body)
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Error while Deleting User: " + bodyData)
+	}
+}
